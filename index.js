7bf2e32391422207b89309458b74a2b0a3b6fa96
bushev <bushevuv@gmail.com>
2015-11-13 14:36:12 +0300

    v1.1.6

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ class Loader extends DioscouriCore.AppBootstrap {
          * @type {string}
          * @private
          */
-        this._moduleVersion = '1.1.5';
+        this._moduleVersion = '1.1.6';
     }
 
     /**
